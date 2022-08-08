4a38751d13f667064d1dda8f4f2180e5890c3b8b
Ekaterina <katya.kinal@gmail.com>
2022-08-08 21:38:08 +0300

    Modified Ex10

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -1,9 +1,14 @@
 ﻿
-Console.Write("Введите число: ");
+/*Console.Write("Введите число: ");
 
 int number = Convert.ToInt32(Console.ReadLine());
 
 int result = (number/100) * 10 + (number % 10);
-Console.WriteLine($"Первая и третья цифры числа: {result}");
+Console.WriteLine($"Первая и третья цифры числа: {result}"); */
+
+string number = Console.ReadLine(); 
+Console.WriteLine(number[0] + "" + number[2]);
+
+
 
 
