5170ab5b21d51f7f0ebbaae05c66d408b305341c
Ekaterina <katya.kinal@gmail.com>
2022-08-08 20:32:56 +0300

    modified Ex09

--- Ex09/Program.cs
+++ Ex09/Program.cs
@@ -4,19 +4,15 @@
 85 -> 8
 */
 
-
-
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Рандомное число: {number}"); // Интерполяция, в отличии от конкатенации, преобразует к выводу, к которому мы привыкли
 
 int firstDigit = number / 10;
 int secondDigit = number %10;
 
-if(firstDigit > secondDigit)
-{
-    Console.WriteLine(firstDigit);
-}
-else
-{
-    Console.WriteLine(secondDigit);
-}
+int max = firstDigit;
+
+if(max < secondDigit) max = secondDigit;
+Console.WriteLine($"Максимальная цифра в числе {number} - это {max}");
+
+
